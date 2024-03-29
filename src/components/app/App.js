025728093ef99431c3a91ff0aba9ebb1bc950b0d
Pavel Popov <pavel2003sbk@yandex.ru>
2024-03-29 22:32:43 +0800

    добавил первоначальный роутинг

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,5 @@
 import { Provider } from 'react-redux';
+import { Redirect, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import store from '../../store';
 import ArticleList from '../article-list/ArticleList';
@@ -6,10 +7,15 @@ import Header from '../header';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Header />
-      <ArticleList />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <Header />
+        <Switch>
+          <Route path="/articles/" component={ArticleList} />
+          <Redirect to="/articles/" />
+        </Switch>
+      </Provider>
+    </Router>
   );
 };
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <header className="header">
-      <h1>Realworld Blog</h1>
+      <Link to="/articles/">
+        <h1>Realworld Blog</h1>
+      </Link>
       <div className="header__button-container">
         <button>Sign in</button>
         <button>Sign Up</button>
