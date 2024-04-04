6fe2a0924a48dcb8654bfe99ac02d8256bf9f51b
Pavel Popov <pavel2003sbk@yandex.ru>
2024-04-04 14:04:35 +0800

    начал работу над  useFieldArray

--- src/components/article-form/ArticleForm.js
+++ src/components/article-form/ArticleForm.js
@@ -37,6 +37,14 @@ const ArticleForm = () => {
                 : null
             }
             {...register('title', {
+              maxLength: {
+                value: 110,
+                message: 'Maximum of 110 characters.',
+              },
+              minLength: {
+                value: 5,
+                message: 'Minimum of 5 characters.',
+              },
               required: 'Title field is require.',
             })}
           />
@@ -47,10 +55,80 @@ const ArticleForm = () => {
               color: '#F5222D',
             }}
           >
-            {errors.email ? errors.email.message : 'text'}
+            {errors.title ? errors.title.message : 'text'}
           </label>
         </div>
-        <button type="submit" className={buttonClasses}>
+        <div>
+          <label htmlFor="title">Short description</label>
+          <input
+            type="text"
+            id="description"
+            placeholder="Description"
+            value={watch('description', '')}
+            style={
+              errors.description
+                ? {
+                    outline: 'none',
+                    borderColor: '#F5222D',
+                  }
+                : null
+            }
+            {...register('description', {
+              required: 'Description field is require.',
+              maxLength: {
+                value: 250,
+                message: 'Maximum of 250 characters.',
+              },
+              minLength: {
+                value: 5,
+                message: 'Minimum of 5 characters.',
+              },
+            })}
+          />
+          <label
+            htmlFor="title"
+            style={{
+              visibility: errors.description ? 'visible' : 'hidden',
+              color: '#F5222D',
+            }}
+          >
+            {errors.description ? errors.description.message : 'text'}
+          </label>
+        </div>
+        <div>
+          <label htmlFor="title">Text</label>
+          <textarea
+            type="text"
+            id="text"
+            placeholder="Text"
+            value={watch('text', '')}
+            style={
+              errors.text
+                ? {
+                    outline: 'none',
+                    borderColor: '#F5222D',
+                  }
+                : null
+            }
+            {...register('text', {
+              required: 'Text field is require.',
+              minLength: {
+                value: 10,
+                message: 'Minimum of 10 characters.',
+              },
+            })}
+          />
+          <label
+            htmlFor="title"
+            style={{
+              visibility: errors.text ? 'visible' : 'hidden',
+              color: '#F5222D',
+            }}
+          >
+            {errors.text ? errors.text.message : 'text'}
+          </label>
+        </div>
+        <button type="submit" disabled={Object.keys(dirtyFields).length === 0} className={buttonClasses}>
           Send
         </button>
       </form>
